8e4931b0a2da6005774361c2995bc2472f1d8607
Dmitriy Machin <dmitriy.m@payop.com>
2018-12-17 09:02:41 +0200

    MAke pywaves visible only from containers

--- docker/dev/pywaves/docker-entrypoint.sh
+++ docker/dev/pywaves/docker-entrypoint.sh
@@ -5,4 +5,5 @@ set -e
 export FLASK_ENV=development
 export FLASK_APP=app.py
 
-python -m flask run
+#python -m flask run --host=0.0.0.0 --port=5000
+gunicorn -b "wifa.pywaves:5000" --access-logfile - "app:app"
